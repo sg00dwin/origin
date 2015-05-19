6fe0b18aad1dd90c15f30b8dfb30934a0c135f81
Jordan Liggitt <jliggitt@redhat.com>
2015-05-19 04:41:18 -0400

    Change config.js back to localhost

--- assets/app/config.js
+++ assets/app/config.js
@@ -4,16 +4,16 @@
 window.OPENSHIFT_CONFIG = {
   api: {
     openshift: {
-      hostPort: "ec2-54-224-141-59.compute-1.amazonaws.com:8443",
+      hostPort: "localhost:8443",
       prefix: "/osapi"
     },
     k8s: {
-      hostPort: "ec2-54-224-141-59.compute-1.amazonaws.com:8443",
+      hostPort: "localhost:8443",
       prefix: "/api"
     }
   },
   auth: {
-  	oauth_authorize_uri: "https://ec2-54-224-141-59.compute-1.amazonaws.com:8443/oauth/authorize",
+  	oauth_authorize_uri: "https://localhost:8443/oauth/authorize",
   	oauth_redirect_base: "https://localhost:9000",
   	oauth_client_id: "openshift-web-console",
   	logout_uri: ""
